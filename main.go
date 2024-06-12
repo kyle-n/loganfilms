8b5fcaf7e6024cfcdb720cc8757521d064b458bc
Kyle Nazario <knazario@bitovi.com>
2024-06-12 14:55:29 -0600

    Removes colley

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 	"time"
@@ -8,16 +9,25 @@ import (
 
 type Showtime struct {
 	Title     string
-	StartTime time.Time
+	ShowTimes []time.Time
 	TMDBID    int
 }
 
 func homepageHandler(w http.ResponseWriter, r *http.Request) {
+	// getMegaplexShowtimes("https://www.megaplextheatres.com/university")
+	getMegaplexShowtimes("0008")
 	w.Write([]byte("Hello, World!"))
+}
+
+func getMegaplexShowtimes(theaterId string) []Showtime {
+	const url = "https://apiv2.megaplextheatres.com/api/film/cinemaFilms" + theaterId
+	showtimes, err := http.Get(url)
+
+	return showTimes
 }
 
 func main() {
 	http.HandleFunc("/", homepageHandler)
-	log.Default().Println("Server is running on port 8080...")
+	fmt.Println("Server is running on port 8080...")
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
